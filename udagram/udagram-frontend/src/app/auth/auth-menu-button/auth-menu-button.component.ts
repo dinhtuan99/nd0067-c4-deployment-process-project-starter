dbd6ddc2dc2c1b6d63c28c85257b430eb9a365f5
dinhtuan99 <vdt12319999@gmail.com>
2023-05-06 23:47:13 +0700

    changes

--- udagram/udagram-frontend/src/app/auth/auth-menu-button/auth-menu-button.component.ts
+++ udagram/udagram-frontend/src/app/auth/auth-menu-button/auth-menu-button.component.ts
@@ -14,7 +14,7 @@ import { AuthRegisterComponent } from '../auth-register/auth-register.component'
 export class AuthMenuButtonComponent implements OnInit {
 
   constructor(
-    private auth: AuthService,
+    readonly auth: AuthService,
     public modalController: ModalController
     ) {}
 
