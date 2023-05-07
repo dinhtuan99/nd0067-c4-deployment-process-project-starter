a6d1e2c19f79799c53cddcc82cb404ddb0dd34fa
dinhtuan99 <vdt12319999@gmail.com>
2023-05-07 12:34:07 +0700

    change env

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: 'Udagram',
-  apiHost: 'https://udagram-api-fpt-dev.us-east-1.elasticbeanstalk.com/api/v0',
+  apiHost: 'http://udagram-api-fpt-dev.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
