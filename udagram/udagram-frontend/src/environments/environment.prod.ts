928b8cb6f96192541acd56e79f43758bdb749988
dinhtuan99 <vdt12319999@gmail.com>
2023-05-07 10:34:09 +0700

    fix code smell

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: true,
-  appName: "Udagram",
-  apiHost: "httpw://udagram-api-tuanvd-dev.us-east-1.elasticbeanstalk.com/api/v0",
+  appName: 'Udagram',
+  apiHost: 'httpw://udagram-api-tuanvd-dev.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
